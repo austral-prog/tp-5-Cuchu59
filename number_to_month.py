a9e9461777ea238131cfc03bff614f039b5b462e
Cuchu Seré <69125170+Cuchu59@users.noreply.github.com>
2024-04-19 11:16:15 -0400

    Update number_to_month.py

--- number_to_month.py
+++ number_to_month.py
@@ -1,6 +1,6 @@
 def number_to_month(month):
     
-    mon_list = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+    mon_list = ["Enero", "diciembre", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
     
     a = mon_list[month_Num - 1] if (month > 0 and month < 13) else "error"
     return a.lower()
