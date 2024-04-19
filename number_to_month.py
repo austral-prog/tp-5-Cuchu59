ccf7c99900c3e890141c0d11c05f2275b05a1c61
Cuchu Seré <69125170+Cuchu59@users.noreply.github.com>
2024-04-19 10:23:05 -0400

    Update number_to_month.py

--- number_to_month.py
+++ number_to_month.py
@@ -1,4 +1,11 @@
 # Replace the "ANSWER HERE" for your answer
 
-def number_to_month(month):
-    return "ANSWER HERE" # Remove this line and implement
+def number_to_month(month_Num):
+    month = ""
+    mon_list = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+    
+    if(month_Num <+ 0 or month_Num > len(mon_list)):
+        return f"error"
+    else:
+        month = mon_list[month_Num - 1]
+        return month
